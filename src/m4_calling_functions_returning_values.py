05591df0321fe2b0172723e1811a066a085ec411
baileykc <baileykc@rose-hulman.edu>
2019-01-28 08:46:51 -0500

    m1 of hw5

--- src/m4_calling_functions_returning_values.py
+++ src/m4_calling_functions_returning_values.py
@@ -7,8 +7,8 @@ This module demonstrates and practices:
   -- UNIT TESTING.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher, Mark Hays,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Killian Bailey.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import m4t_tester
 
@@ -74,7 +74,11 @@ def run_test_sum_of_digits():
     # -------------------------------------------------------------------------
 
 
-def sum_of_digits(number):
+
+def sum_of_digits():
+
+
+
     """
     What comes in:  An integer.
     What goes out:  The sum of the digits in the given integer.
